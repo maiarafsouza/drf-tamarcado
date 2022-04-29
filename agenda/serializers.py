16395a260426d092414935b793386e3b77a49c03
Maiara Souza <contact.maiarasouza@gmail.com>
2022-04-29 19:54:14 -0300

    Add tests and date validation

--- agenda/serializers.py
+++ agenda/serializers.py
@@ -1,16 +1,58 @@
+import imp
+import re
 from rest_framework import serializers
+from datetime import datetime
+from django.utils import timezone
 from agenda.models import Agendamento
 
-class AgendamentoSerializer(serializers.Serializer):
-    data_horario = serializers.DateTimeField()
-    nome_cliente = serializers.CharField(max_length=200)
-    email_cliente = serializers.EmailField()
-    telefone_cliente = serializers.CharField(max_length=20)
+class AgendamentoSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Agendamento
+        fields = ['id', 'data_horario', 'nome_cliente', 'email_cliente', 'telefone_cliente']  # datetime: YYYY-MM-DD-Thh:mm
+
+    def validate_data_horario(self, value):
+        if value < timezone.now():
+            raise serializers.ValidationError('Agendamento só pode ser feito para futuro!')
+        return value
+    
+    def validate_telefone_cliente(self, attrs):
+        telefone_cliente = attrs
+        invalid_characters = re.compile(r'[^\d+( )]')
+        invalid_telephone = invalid_characters.findall(telefone_cliente)
+        
+        if len(telefone_cliente) < 8:
+            raise serializers.ValidationError('Telefone deve ter pelo menos 8 caracteres')
+        
+        if invalid_telephone != []:
+            raise serializers.ValidationError(f'Caracteres {invalid_telephone} não permitidos')
+
+        if '+' in telefone_cliente and '+' in telefone_cliente[1:]:
+                raise serializers.ValidationError('Número inválido')
+        
+        return attrs
+    
+    def validate(self, attrs):
+        email_cliente = attrs.get('email_cliente', '')
+        telefone_cliente = attrs.get('telefone_cliente', '')
+        data_horario = attrs.get('data_horario', '')
+
+
+        if email_cliente.endswith('.br') and telefone_cliente.startswith('+') and not telefone_cliente.startswith('+55'):
+            raise serializers.ValidationError('Email brasileiro deve estar associado a telefone brasileiro (+55)')
+
+        data_agendamento = data_horario.date()
+        agendamentos_cliente = Agendamento.objects.filter(email_cliente=email_cliente, data_horario__date=data_agendamento, status_cancelado=False)
+        if list(agendamentos_cliente.values()) != []:
+            raise serializers.ValidationError('Usuário já tem agendamento nesta data')
+
+        return attrs
 
     # When only validated_data is passed from view to function, the create function is used
     # When instance and validated_data are passed from view to function, the update function is used
-    
-    def create(self, validated_data):
+    # Model Serializers have create and update methods by default
+
+    # def create(self, validated_data):
         agendamento = Agendamento.instance.create(
                 data_horario = validated_data['data_horario'],
                 nome_cliente = validated_data['nome_cliente'],
@@ -19,7 +61,7 @@ class AgendamentoSerializer(serializers.Serializer):
             ) # Creates Agendamento instance from view
         return agendamento
     
-    def update(self, instance, validated_data):
+    # def update(self, instance, validated_data):
         #If data_horario isn't found in dict, the previous value of instance.data_horario (standard value argument) is persisted in memory
         instance.data_horario = validated_data.get('data_horario', instance.data_horario)
         instance.nome_cliente = validated_data.get('nome_cliente', instance.nome_cliente)
--- agenda/tests.py
+++ agenda/tests.py
@@ -1,3 +1,80 @@
-from django.test import TestCase
+from datetime import datetime, timezone
+import json
+from urllib import request, response
+from rest_framework.test import APITestCase
+from agenda.models import Agendamento
 
 # Create your tests here.
+# json.dumps(): recieve python data structure (dict) and transforms into json dict
+# json.loads(): receive json format (incliding binary) and transforms into python data structure
+
+class TestListaAgendamentos(APITestCase):
+    def test_listagem_vazia(self):
+        response = self.client.get('/api/agendamentos/') # binary format
+        data = json.loads(response.content)
+        self.assertEqual(data, [])
+
+    def test_listagem_agendamentos_criados(self):
+        Agendamento.objects.create(
+            data_horario=datetime(2023, 3, 15, tzinfo=timezone.utc),
+            nome_cliente='Alice',
+            email_cliente='alice@gmail.com',
+            telefone_cliente='99998888',
+        )
+        
+        agendamento_serializado = {
+            'id': 1,
+            'data_horario': '2023-03-15T00:00:00Z',
+            'nome_cliente': 'Alice',
+            'email_cliente': 'alice@gmail.com',
+            'telefone_cliente': '99998888',
+        }
+
+        response = self.client.get('/api/agendamentos/')
+        data = json.loads(response.content)
+        self.assertDictEqual(data[0], agendamento_serializado)
+
+class TestCriaAgendamento(APITestCase):
+    def test_cria_agendamento(self):
+        agendamento_request_data = {
+            'data_horario': '2023-03-16T00:00:00Z',
+            'nome_cliente': 'Alice',
+            'email_cliente': 'alice@gmail.com',
+            'telefone_cliente': '99998888',
+        }
+        request = self.client.post('/api/agendamentos/', agendamento_request_data, format='json')
+        response = self.client.get('/api/agendamentos/')
+        response_data = json.loads(response.content)
+
+        self.assertEqual(response_data[0]['data_horario'], '2023-03-16T00:00:00Z')
+        self.assertEqual(response_data[0]['nome_cliente'], 'Alice')
+        self.assertEqual(response_data[0]['email_cliente'], 'alice@gmail.com')
+        self.assertEqual(response_data[0]['telefone_cliente'], '99998888')
+    
+    def test_request_inválido_retorna_400(self):
+        agendamento_request_data = {
+            'data_horario': '2023-03-17T00:00:00Z',
+            'nome_cliente': 'Alice',
+            'email_cliente': 'alice@gmail.com.br',
+            'telefone_cliente': '+1999abc98888',
+        }
+        response = self.client.post('/api/agendamentos/', agendamento_request_data, format='json')
+        self.assertEqual(response.status_code, 400)
+    
+    def test_agendamentos_mesma_data(self):
+        agendamento1_request_data = {
+            'data_horario': '2023-03-18T12:00:00Z',
+            'nome_cliente': 'Alice',
+            'email_cliente': 'alice@gmail.com.br',
+            'telefone_cliente': '+1999abc98888',
+        }
+        agendamento2_request_data = {
+            'data_horario': '2023-03-18T13:00:00Z',
+            'nome_cliente': 'Alice',
+            'email_cliente': 'alice@gmail.com.br',
+            'telefone_cliente': '+1999abc98888',
+        }
+        response1 = self.client.post('/api/agendamentos/', agendamento1_request_data, format='json')
+        response2 = self.client.post('/api/agendamentos/', agendamento2_request_data, format='json')
+        self.assertEqual(response2.status_code, 400)
+
